ad4b570fa6f3c558bbd01383dd5932ab0d6d7e41
Tianxin Liu <liutianxin@runchinamarathon.com>
2021-09-27 23:04:26 -0400

    merge

--- client/src/Signup.js
+++ client/src/Signup.js
@@ -3,7 +3,7 @@ import { Redirect, useHistory } from "react-router-dom";
 import { connect } from "react-redux";
 import {
   Grid,
-  Box,
+  Hidden,
   Typography,
   Button,
   FormControl,
@@ -11,9 +11,13 @@ import {
   FormHelperText,
 } from "@material-ui/core";
 import { register } from "./store/utils/thunkCreators";
+import SideView from "./components/AuthPages/SideView";
+import HeaderContent from "./components/AuthPages/HeaderContent";
+import { useAuthStyles } from './components/AuthPages/styles';
 
 const Login = (props) => {
   const history = useHistory();
+  const authClasses = useAuthStyles();
   const { user, register } = props;
   const [formErrorMessage, setFormErrorMessage] = useState({});
 
@@ -37,69 +41,6 @@ const Login = (props) => {
   }
 
   return (
-<<<<<<< HEAD
-    <Grid container justify="center">
-      <Box>
-        <Grid container item>
-          <Typography>Need to log in?</Typography>
-          <Button onClick={() => history.push("/login")}>Login</Button>
-        </Grid>
-        <form onSubmit={handleRegister}>
-          <Grid>
-            <Grid>
-              <FormControl>
-                <TextField
-                  aria-label="username"
-                  label="Username"
-                  name="username"
-                  type="text"
-                  required
-                />
-              </FormControl>
-            </Grid>
-            <Grid>
-              <FormControl>
-                <TextField
-                  label="E-mail address"
-                  aria-label="e-mail address"
-                  type="email"
-                  name="email"
-                  required
-                />
-              </FormControl>
-            </Grid>
-            <Grid>
-              <FormControl error={!!formErrorMessage.confirmPassword}>
-                <TextField
-                  aria-label="password"
-                  label="Password"
-                  type="password"
-                  inputProps={{ minLength: 6 }}
-                  name="password"
-                  required
-                />
-                <FormHelperText>
-                  {formErrorMessage.confirmPassword}
-                </FormHelperText>
-              </FormControl>
-            </Grid>
-            <Grid>
-              <FormControl error={!!formErrorMessage.confirmPassword}>
-                <TextField
-                  label="Confirm Password"
-                  aria-label="confirm password"
-                  type="password"
-                  inputProps={{ minLength: 6 }}
-                  name="confirmPassword"
-                  required
-                />
-                <FormHelperText>
-                  {formErrorMessage.confirmPassword}
-                </FormHelperText>
-              </FormControl>
-            </Grid>
-            <Button type="submit" variant="contained" size="large">
-=======
     <Grid container justifyContent="center" alignItems="center" spacing={0} className={authClasses.root}>
       <Hidden mdDown>
         <Grid container item={true} md={12} lg={5} justifyContent="center" className={authClasses.side}>
@@ -171,12 +112,11 @@ const Login = (props) => {
               variant="contained"
               color="primary"
               className={authClasses.submitButton}>
->>>>>>> develop
               Create
-            </Button>
-          </Grid>
-        </form>
-      </Box>
+              </Button>
+          </form>
+        </Grid>
+      </Grid>
     </Grid>
   );
 };
